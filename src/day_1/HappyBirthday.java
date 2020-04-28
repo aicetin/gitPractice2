d51475871f24df5abb31186db124f46106eedf26
aicetin <61978685+aicetin@users.noreply.github.com>
2020-04-28 15:51:03 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -4,5 +4,7 @@ public class HappyBirthday {
     public static void main(String[] args) {
 
         System.out.println("Happy birthday B18");
+        System.out.println("Happy birthday my friend");
+            
     }
 }
